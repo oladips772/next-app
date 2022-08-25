6576226dd09ffee9d930b3b33ad3b8e17d915140
oladips <oladips200@gmail.com>
2022-08-25 20:26:52 +0000

    creating get static props function

---
+++ pages/coins/index.js
@@ -0,0 +1,18 @@
+/** @format */
+import axios from "axios";
+
+const CoinList = () => {
+    const URL = "https://api.coinstats.app/public/v1/coins?skip=0";
+
+  return (
+    <>
+      <div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export 
+
+export default CoinList;
